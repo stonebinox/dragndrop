65718add0bde181de811c92b493de97a68bf6ca5
stonebinox <anoop.santhanam@gmail.com>
2017-11-20 12:34:49 +0530

    Field for item description

--- web/js/dragndrop.js
+++ web/js/dragndrop.js
@@ -233,7 +233,7 @@ app.controller('dd', function($scope,$compile,$http){
                 var filesize=properties[1];
                 filesize=filesize[1];
                 var filename=properties[0][1];
-                table+='<tr><td>'+filename+'</td><td>'+filesize+'</td><td><div class="btn-group" id="item'+i+'"><button type="button" class="btn btn-primary btn-xs" ng-click="uploadItem('+i+')">Upload</button><button type="button" class="btn btn-default btn-xs" ng-click="removeItem('+i+')">Remove</button></div></td></tr>';
+                table+='<tr><td>'+filename+'</td><td>'+filesize+'</td><td><div class="btn-group" id="item'+i+'"><button type="button" class="btn btn-info btn-xs">Add description</button><button type="button" class="btn btn-primary btn-xs" ng-click="uploadItem('+i+')">Upload</button><button type="button" class="btn btn-default btn-xs" ng-click="removeItem('+i+')">Remove</button></div></td></tr>';
             }
             table+='</tbody></table>';
             $("#itemlist").html(table);
